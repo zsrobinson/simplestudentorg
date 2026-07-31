2a66dc7e443f7d71a4cad403aec659aa344dd641
Zachary Robinson <me@zsrobinson.com>
2026-07-31 12:29:56 -0400

    set up cloudflare email

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,10 @@
+import { env } from "cloudflare:workers";
 import { drizzleAdapter } from "@better-auth/drizzle-adapter/relations-v2";
 import { betterAuth } from "better-auth";
 import { magicLink } from "better-auth/plugins";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
-import { account, session, user, verification } from "~/db/auth-schema";
 import { db } from "~/db";
+import { account, session, user, verification } from "~/db/auth-schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -14,13 +15,16 @@ export const auth = betterAuth({
   // tanstackStartCookies should be last in the array, apparently
   plugins: [
     magicLink({
-      sendMagicLink: async ({ email, token, url, metadata }, ctx) => {
-        console.log("# MAGIC LINK");
-        console.log("  - email:", email);
-        console.log("  - token:", token);
-        console.log("  - url:", url);
-        console.log("  - metadata:", metadata);
-        console.log("  - ctx:", ctx);
+      sendMagicLink: async ({ email, url }) => {
+        const { messageId } = await env.EMAIL.send({
+          to: email,
+          from: "login@simplestudent.org",
+          subject: "Your magic link for Simple Student Org",
+          html: `<h1>Simple Student Org</h1>
+            <p>To log in, follow this link:</p>
+            <a href="${url}">${url}</a>`,
+        });
+        console.log("Send magic link email", { messageId });
       },
     }),
     tanstackStartCookies(),
